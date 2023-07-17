3fc5ca64251986150f88abe0c6c5cb1043aa32fc
zhangb926 <zhangb@yusur.tech>
2023-07-17 17:48:52 +0800

    第二次修改

---
+++ index.c
@@ -0,0 +1,6 @@
+/**
+ * @Author: zhangb
+ * @date: 2023-07-17 17:34:06
+ * @last_author: zhangb (zhangb@yusur.tech)
+ * @last_edit_time: 2023-07-17 17:34:06
+ */
--- index.cpp
+++ index.cpp
@@ -2,6 +2,7 @@
  * @Author: zhangb
  * @date: 2023-07-17 17:03:18
  * @last_author: zhangb (zhangb@yusur.tech)
- * @last_edit_time: 2023-07-17 17:03:18
+ * @last_edit_time: 2023-07-17 17:32:38
  */
 #include <iostream>
+using namespace std;
